4d643a8adc6fbfe919a5ddb4ced2ac2a73693c02
Igor Araujo Matos <sarkan.igor@gmail.com>
2022-09-22 21:11:54 -0300

    Correção

--- public/js/home.js
+++ public/js/home.js
@@ -126,7 +126,7 @@ function getCashOut() {
                 <div class="container p-0">
                     <div class="row">
                         <div class="col-12 col-md-8">
-                            <p>${cashIn[index].description}/</p>
+                            <p>${cashIn[index].description}</p>
                         </div>
                         <div class="col-12 col-md-3 d-flex justify-content-end">
                             ${cashIn[index].date}
